e4de8df2378c031f8e9f1e3eff3283f59405dd4d
couttskate <katecou@pnghs.school.nz>
2022-06-17 09:42:37 +1200

    Version 1 string checker complete

--- 01_string_check_v1.py
+++ 01_string_check_v1.py
@@ -1,6 +1,24 @@
 # string checker function
 # v1 - create basic string checker
+# works correctly as of 17/6/22
 
 # function goes here
+def string_checker(question):
 
-# main routine
+    valid = False
+    while not valid:
+
+        response = input(question).title().strip()
+
+        if response != "":
+            return response
+            break
+
+        else:
+            print("sorry that is not a valid response")
+            print()
+
+# *** Main Routine starts here ***
+recipe_name = string_checker("What is your recipe called?")
+print("Your recipe is: ", recipe_name)
+print()
