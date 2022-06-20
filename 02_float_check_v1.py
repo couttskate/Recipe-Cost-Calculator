0500fdd6c3e769780cedfc5bd09c1b142ba1450f
couttskate <katecou@pnghs.school.nz>
2022-06-20 14:47:44 +1200

    Created float checker, added to base and created new version of base for adding more components onto.

--- 02_float_check_v1.py
+++ 02_float_check_v1.py
@@ -1,4 +1,25 @@
 # float checker function
 # v1 - checks that user input is a valid float
+# works correctly as of @20/6/22
 # function goes here
-# main routine
+def float_check(question):
+    error = "This is not a valid number."
+    valid = False
+    while not valid:
+
+        # ask user for number and check if it is valid
+        try:
+            response = float(input(question))
+            return response
+
+        # if an integer is not entered, display an error
+        except ValueError:
+            print(error)
+
+# main routine goes here
+# get serving size
+serving_size = float_check("Please enter the serving size for this recipe: ")
+
+
+
+
